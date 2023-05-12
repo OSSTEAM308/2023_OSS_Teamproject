a8675e3f2b1fd544906e0e0871df2c772eefaedc
Enos <otj111000@naver.com>
2023-05-12 19:59:10 +0900

    [Feat] add fun in Book.c

--- Book.c
+++ Book.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include"Book.h"
 
-
 void DisplayBook(Book ** book_data)
 {  
     printf("no    Title    author     publisher     end_date\n");
@@ -15,7 +14,7 @@ void DisplayBook(Book ** book_data)
         {
             if(book_data[i]->status != -1)
             {
-                printf("%d    %s       %s  %s  %s\n",book_data[i]->no, book_data[i]->book_title, book_data[i]->author, book_data[i]->publisher, book_data[i]->end_date);
+                printf("%d    %s       %s  %s  %s\n",book_data[i]->no, book_data[i]->title, book_data[i]->author, book_data[i]->publisher, book_data[i]->end_date);
                 flag++;
             }           
         }
@@ -23,42 +22,48 @@ void DisplayBook(Book ** book_data)
     printf("-> %d data\n", data_num);
 }
  
-void addBook(Book ** book_data)
+void AddBook(Book ** book_data)
 {
     idx++;
     data_num++;
     book_data[idx] = (Book *)malloc(sizeof(Book));
     getchar();
 
-    printf("번호 : ");
-    fgets(book_data[idx]->no, LEN_NAME, stdin);
-    book_data[idx]->name[strlen(book_data[idx]->name) - 1] = '\0';
+    printf("Title : ");
+    fgets(book_data[idx]->title, LEN_TITLE, stdin);
+    book_data[idx]->title[strlen(book_data[idx]->title) - 1] = '\0';
 
-    printf("번호 : ");
-    fgets(book_data[idx]->book_dataNum, LEN_book_data, stdin);
-    book_data[idx]->book_dataNum[strlen(book_data[idx]->book_dataNum) - 1] = '\0';
+    printf("Author : ");
+    fgets(book_data[idx]->author, LEN_AUTHOR, stdin);
+    book_data[idx]->author[strlen(book_data[idx]->author) - 1] = '\0';
 
-    printf("관계 : ");
-    fgets(book_data[idx]->relationship, LEN_REL, stdin);
-    book_data[idx]->relationship[strlen(book_data[idx]->relationship) - 1] = '\0';
+    printf("Publisher : ");
+    fgets(book_data[idx]->, LEN_PUBLISHER, stdin);
+    book_data[idx]->publisher[strlen(book_data[idx]->publisher) - 1] = '\0';
 
-    printf("년생 : ");
-    fgets(book_data[idx]->birth, LEN_BIRTH, stdin);
-    book_data[idx]->birth[strlen(book_data[idx]->birth) - 1] = '\0';
+    printf("Publisher : ");
+    fgets(book_data[idx]->publisher, LEN_PUBLISHER, stdin);
+    book_data[idx]->publisher[strlen(book_data[idx]->publisher) - 1] = '\0';
 
-    book_data[idx]->num = no++;
+    printf("End_date : ");
+    fgets(book_data[idx]->end_date, LEN_DATES, stdin);
+    book_data[idx]->end_date[strlen(book_data[idx]->end_date) - 1] = '\0';
+
+    book_data[idx]->no = no++;
 }
 
-void deletebook_data(Data **book_data)
+void DeleteBook(Book **book_data)
 {
-    readbook_data(book_data);
+    DisplayBook(book_data);
+
     int num,flag=0;
-    printf("\n삭제할 번호 -> ");
+
+    printf("\nDelete No -> ");
     scanf("%d", &num);
 
     for (int i = 0; i <= idx;i++)
     {
-        if(book_data[i]->num == num && book_data[i]->status != -1)
+        if(book_data[i]->no == num && book_data[i]->status != -1)
         {
             flag++;
             book_data[i]->status = -1;
@@ -71,26 +76,26 @@ void deletebook_data(Data **book_data)
         printf("없는 번호\n");
 }
 
-void searchbook_data(Data **book_data)
+void SearchBook(Book **book_data)
 {
-    char temp[LEN_NAME];
-    printf("\n찾을 사람의 이름 -> ");
+    char temp[LEN_TITLE];
+    printf("\nBook title -> ");
     getchar();
     fgets(temp,sizeof(temp), stdin);
     temp[strlen(temp) - 1] = '\0';
    
-    printf("번호  이름      핸드폰번호  관계    년생\n");
+    printf("no    Title    author     publisher     end_date\n");
     printf("---------------------------------------------------------------------\n");
     int flag = 0;
     for (int i = 0; i <= idx;i++)
     {
-        if(strstr(book_data[i]->name,temp) && book_data[i]->status != -1)
+        if(strstr(book_data[i]->title,temp) && book_data[i]->status != -1)
         {
             flag++;
-            printf("%d    %s       %s  %s  %s\n",book_data[i]->num, book_data[i]->name, book_data[i]->book_dataNum, book_data[i]->relationship, book_data[i]->birth);
+             printf("%d    %s       %s  %s  %s\n",book_data[i]->no, book_data[i]->title, book_data[i]->author, book_data[i]->publisher, book_data[i]->end_date);
         }
     }
 
     if(flag ==0)
-        printf("해당 데이터를 찾을수 없습니다\n");
+        printf("No Data\n");
 }
