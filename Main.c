64577e134a5b211044c2a0859af996660cd38d67
ehdgusqus <ehdgusqus55@gmail.com>
2023-05-20 21:40:34 +0900

    [Fix] Main.c

--- Main.c
+++ Main.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
-#include "Book.h"
-#include "Member.h"
+#include"Book.h"
 
 #define MAX_BOOKDATA 100
-
 
 int main() 
 {
     int choice;
     Book *book_data[MAX_BOOKDATA];
-    Member *data_member = NULL;
 
     while (1) 
     {
@@ -31,50 +28,48 @@ int main()
 
         scanf("%d", &choice);
 
-        switch (choice) 
-        {
-            case 1:
-                DisplayBook(book_data);
-                break;
-            case 2:
-                AddBook(book_data);
-                break;
-            case 3:
-                DeleteBook(book_data);
-                break;
-            case 4:
-                ModifyBook(book_data);
-                break;
-            case 5:
-                SearchBook(book_data);
-                break;
-            case 6:
-                LoanBook(book_data);
-                break;
-            case 7:
-                ReturnBook(book_data);
-                break;
-            case 8:
-                DisplayMember(data_member);
-                break;
-            case 9:
-                AddMember(&data_member);
-                break;
-            case 10:
-                DeleteMember(&data_member);
-                break;
-            case 11:
-                SearchMember(data_member);
-                break;
-            case 12:
-                SaveMemberdata(data_member);
-                printf("Thank you for using the library management system.\n");
-                exit(0);
-            default:
-                printf("Invalid choice. Please enter a number between 1 and 12.\n");
+    switch (choice) 
+	{
+        case 1:
+            DisplayBook(book_data);
+            break;
+        case 2:
+            AddBook(book_data);
+            break;
+        case 3:
+            DeleteBook(book_data);
+            break;
+        case 4:
+            ModifyBook(book_data);
+            break;
+        case 5:
+            SearchBook(book_data);
+            break;
+        case 6:
+            LoanBook(book_data);
+            break;
+        case 7:
+            ReturnBook(book_data);
+            break;
+      /*  case 8:
+            DisplayMember();
+            break;
+        case 9:
+            AddMember();
+            break;
+        case 10:
+            DeleteMember();
+            break;
+        case 11:
+            SearchMember();
+            break; */
+        case 12:
+            printf("Thank you for using the library management system.\n");
+            exit(0);
+        default:
+            printf("Invalid choice. Please enter a number between 1 and 12.\n");
         }
     }
-
     return 0;
 }
 
