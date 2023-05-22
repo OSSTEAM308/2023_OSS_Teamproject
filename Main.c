10dea0d26ebfaf04e0dee81ca5a21889417d34b0
ehdgusqus <ehdgusqus55@gmail.com>
2023-05-22 16:55:00 +0900

    [Feat] (fun) LoadBook, LoadMember in Main.c

--- Main.c
+++ Main.c
@@ -6,33 +6,45 @@
 #define MAX_MEMBERDATA 100
 #define MAX_BOOKDATA 100
 
+int LoadBook(Book **book_data);
+int LoadMember(Member **Member_data);
+
 int main()
 {
     int choice;
     Book *book_data[MAX_BOOKDATA];
     Member *member_data [MAX_MEMBERDATA];
 
+    idx = LoadBook(book_data);
+    num_members = LoadMember(member_data);
+    if(idx > 0)
+        no = book_data[idx - 1]->no;
+
     while (1) {
-        printf("\nLibrary Management System\n");
-        printf("0. Exit\n");
-        printf("1. Display book list\n");
-        printf("2. Add a book\n");
-        printf("3. Delete a book\n");
-        printf("4. Edit a book\n");
-        printf("5. Search a book\n");
-        printf("6. Loan a book\n");
-        printf("7. Return a book\n");
-        printf("8. Save book\n");
-        printf("9. Display member list\n");
-        printf("10. Add a member\n");
-        printf("11. Delete a member\n");
-        printf("12. Search a member\n");
-        printf("13. Save member\n");
-        printf("Enter your choice (1-13): ");
+        printf("忙式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式忖\n");
+        printf("弛         Library Management System       弛\n");
+        printf("戍式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式扣\n");
+        printf("弛 0. Exit                                 弛\n");
+        printf("弛 1. Display book list                    弛\n");
+        printf("弛 2. Add a book                           弛\n");
+        printf("弛 3. Delete a book                        弛\n");
+        printf("弛 4. Edit a book                          弛\n");
+        printf("弛 5. Search a book                        弛\n");
+        printf("弛 6. Loan a book                          弛\n");
+        printf("弛 7. Return a book                        弛\n");
+        printf("弛 8. Save book                            弛\n");
+        printf("弛 9. Display member list                  弛\n");
+        printf("弛 10. Add a member                        弛\n");
+        printf("弛 11. Delete a member                     弛\n");
+        printf("弛 12. Search a member                     弛\n");
+        printf("弛 13. Save member                         弛\n");
+        printf("戌式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式戎\n");
+        printf("\n");
+        printf("Enter your choice (0-13): ");
 
         scanf("%d", &choice);
         printf("\n");
-
+  
         if(choice == 0)
         {
             printf("Thank you for using the library management system.\n");
@@ -85,4 +97,55 @@ int main()
         }
     }
     return 0;
+}
+
+int LoadBook(Book **book_data) 
+{
+    FILE *file = fopen(BOOK_FILENAME, "r");
+
+    int count = 0;
+      
+    while (!feof(file))
+    {
+        book_data[count] = (Book *)malloc(sizeof(Book));
+        fscanf(file, "%u %s %s %s %s %s",
+               &book_data[count]->no,
+               book_data[count]->title,
+               book_data[count]->author,
+               book_data[count]->publisher,
+               book_data[count]->end_date,
+               book_data[count]->name);
+
+        if(feof(file))
+            break;
+       
+        data_num++;
+        count++;
+    }
+
+    fclose(file);
+    return count;
+}
+
+int LoadMember(Member **member_data) 
+{
+    FILE *file = fopen(MEMBER_FILENAME, "r");
+    
+    int count = 0;
+    while (!feof(file)) 
+    {
+        member_data[count] = (Member *)malloc(sizeof(Member));
+      
+        fscanf(file, "%s %u %s",
+              member_data[count]->name,
+              &member_data[count]->age,
+              member_data[count]->loan);
+
+        if(feof(file))
+                break;
+        count++;
+    }
+
+    fclose(file);
+    return count;
 }
