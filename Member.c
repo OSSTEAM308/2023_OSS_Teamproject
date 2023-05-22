93938ce6bf9b2152f052d1ad1c497c31c6f38ca3
ehdgusqus <ehdgusqus55@gmail.com>
2023-05-22 17:12:54 +0900

    [Fix](fun) fix error SaveMemberData(Member **data_member) in Member.c

--- Member.c
+++ Member.c
@@ -5,8 +5,10 @@
 
 #define MAX_MEMBERS 100
 
-void AddMember(Member **data_member) {
-    if (num_members >= MAX_MEMBERS) {
+void AddMember(Member **data_member)
+ {
+    if (num_members >= MAX_MEMBERS) 
+    {
         printf("Maximum number of members reached.\n");
         return;
     }
@@ -28,30 +30,36 @@ void AddMember(Member **data_member) {
     printf("Member added successfully!\n");
 }
 
-void SearchMember(Member **data_member) {
+void SearchMember(Member **data_member) 
+{
     char search_name[100];
     
     printf("Enter member name to search: ");
+    getchar();
     fgets(search_name, sizeof(search_name), stdin);
-    search_name[strcspn(search_name, "\n")] = '\0';
-    
+    search_name[strlen(search_name) - 1] = '\0';
+
     int found = 0;
-    for (int i = 0; i < num_members; i++) {
-        if (strcmp((data_member[i])->name, search_name) == 0 && data_member[i]->status != -1) {
-            printf("Member found:\n");
-            printf("Name: %s\n", (*data_member)->name);
-            printf("Age: %u\n", (*data_member)->age);
+
+    for (int i = 0; i < num_members; i++) 
+    {
+        if(strstr(data_member[i]->name,search_name) && data_member[i]->status != -1)
+        {
+            printf("Member found\n");
+            printf("Name: %s ", data_member[i]->name);
+            printf("Age: %u ", data_member[i]->age);
+            printf("Loaned book: %s \n", data_member[i]->loan);
             found = 1;
             break;
         }
     }
     
-    if (!found) {
+    if (!found) 
         printf("Member not found.\n");
-    }
 }
 
-void DeleteMember(Member **data_member) {
+void DeleteMember(Member **data_member)
+{
     char delete_name[100];
     getchar();
 
@@ -60,8 +68,10 @@ void DeleteMember(Member **data_member) {
     delete_name[strcspn(delete_name, "\n")] = '\0';
     
     int found = 0;
-    for (int i = 0; i < num_members; i++) {
-        if (strcmp((data_member[i])->name, delete_name) == 0) {
+    for (int i = 0; i < num_members; i++)
+    {
+        if (strcmp((data_member[i])->name, delete_name) == 0) 
+        {
             data_member[i]->status = -1;
             found = 1;
             printf("Member deleted successfully.\n");
@@ -69,36 +79,47 @@ void DeleteMember(Member **data_member) {
         }
     }
     
-    if (!found) {
+    if (!found) 
         printf("Member not found.\n");
-    }
 }
 
-void DisplayMember(Member **data_member) {
+void DisplayMember(Member **data_member)
+{
     printf("Member List:\n");
     printf("--------------------------------------------------------------\n");
     printf("Name\t\tAge\t\tbooks on loan\n");
     printf("--------------------------------------------------------------\n");
     
-    for (int i = 0; i < num_members; i++) {
+    for (int i = 0; i < num_members; i++) 
+    {
         if(data_member[i]->status != -1)
-            printf("%s\t\t%u\t\t%s\n", data_member[i]->name, data_member[i]->age,data_member[i]->Loan);
+            printf("%s\t\t%u\t\t%s\n", data_member[i]->name, data_member[i]->age,data_member[i]->loan);
     }
     
     printf("--------------------------------------------------------------\n");
 }
 
-void SaveMemberData(Member **data_member) {
+void SaveMemberData(Member **data_member) 
+{
     FILE *file = fopen(MEMBER_FILENAME, "w");
-    if (file == NULL) {
+    if (file == NULL) 
+    {
         printf("Error opening file.\n");
         return;
     }
     
-    for (int i = 0; i < num_members; i++) {
-        fprintf(file, "%s,%u,%s\n", (*data_member)->name, (*data_member)->age, (*data_member)->Loan);
+    for (int i = 0; i < num_members; i++) 
+    {
+        if(data_member[i]->status != -1)
+        {
+            fprintf(file, "%s %u ", data_member[i]->name, data_member[i]->age);
+            if(strlen(data_member[i]->loan) < 1)
+                fprintf(file, "x\n");
+            else
+                fprintf(file, "%s\n", data_member[i]->loan);
+        }
+            
     }
-    
     fclose(file);
     printf("Member data saved successfully.\n");
 }
